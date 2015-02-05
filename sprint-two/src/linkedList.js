3db7c6d1672e974162b0852067ef9677f98e28f0
knatsuki92 <knatsuki92@gmail.com>
2015-02-05 11:36:41 -0800

    Finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.tail !== null ){
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+    list[value] = newNode;
+
+    //initial case (empty list)
+    if (list.head === null){
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function(){
+    //case: empty list
+    if(list.head === null){
+      return null;
+    }
+
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    delete list[headValue]; //remove the head Node from the list.
+
+
+    //case: one node in list
+    if (list.head === null) {
+      list.tail = null;
+    }
+
+    return headValue;
   };
 
   list.contains = function(target){
+    var isFound = false;
+    for (var key in list){
+      if (list[key].value === target) {
+        isFound = true;
+      }
+    }
+    return isFound;
+
   };
 
   return list;
