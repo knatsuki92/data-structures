b35df0761d6d20d68d536a5421d63786ef6b2a72
knatsuki92 <knatsuki92@gmail.com>
2015-02-05 22:50:15 -0800

    Finished up to graph, not including the graphSpec.js error for removeNode test (graph.js is modified)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,70 @@
 
 
 var Graph = function(){
+  this.storage = [];
 };
 
 Graph.prototype.addNode = function(node){
+  var newNode = {};
+  newNode.node = node;
+  newNode.edge = [];
+
+  this[node] = newNode;
+  this.storage.push(node);
 };
 
 Graph.prototype.contains = function(node){
+  var isFound = false;
+  _.each(this.storage, function(item){
+    if (item === node){
+      isFound = true;
+    }
+  });
+  return isFound;
 };
 
 Graph.prototype.removeNode = function(node){
+  for (var key in this){
+    if (key === node){
+      delete this[key];
+    }
+  }
+  this.storage.splice(this.storage.indexOf(node),1);
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+    // var connected = false;
+    for (var i = 0; i < this[fromNode].edge.length; i++ ){
+      if (this[fromNode].edge[i].node === toNode){
+        return true;
+        // connected = true;
+      }
+    }
+    return false;
+    //return connected;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if (this[toNode] && this[fromNode]){
+    this[fromNode].edge.push(this[toNode]);
+    this[toNode].edge.push(this[fromNode]);
+  }
+
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if (this.hasEdge(fromNode,toNode)){
+    this[fromNode].splice(this[fromNode].edge.indexOf(this[toNode]),1);
+    this[toNode].splice(this[toNode].edge.indexOf(this[fromNode]),1);
+  }
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this){
+    cb(this[key].node);
+  }
 };
 
 /*
