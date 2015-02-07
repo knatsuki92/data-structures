a860cdbc522c2d9cf583724d5dd82a4063731ba2
knatsuki92 <knatsuki92@gmail.com>
2015-02-06 21:17:14 -0800

    hashtable complete.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,43 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // this._storage.each(function(value,index,storage){
+  //   if (!storage[i]){
+  //     storage[i] = this._storage.set(i,v);
+  //   }
+  var bucket = this._storage.get(i);
+  if (bucket === undefined){
+    bucket = [[k,v]];
+  } else {
+    if(this.retrieve(k) === null){
+      bucket.push([k,v]);
+  }
+  this._storage.set(i,bucket);
+
+  });
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if(bucket[j] !== undefined)
+      if(bucket[j][0] === k) {
+       return bucket[j][1];
+      }
+    }
+  }
+  return null;
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(bucket, index, storage){
+    if (index === i){
+      storage[index] = null;
+    }
+  });
 
 };
 
