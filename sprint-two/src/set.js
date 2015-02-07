ded297dfda7506b005e3133ff26b329e52ed6fcc
knatsuki92 <knatsuki92@gmail.com>
2015-02-06 16:10:03 -0800

    Set complete

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (this._storage[item] === undefined){
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item){
+  if (this._storage[item] !== undefined){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if (this._storage[item] !== undefined){
+    delete this._storage[item];
+  }
 };
 
 /*
